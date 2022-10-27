51af07dc8837cffe16687d229f2eb119742665c2
fernandogs07 <fernandogs07@hotmail.com>
2022-10-27 11:53:24 -0300

    Criação página captura

---
+++ estudo.js
@@ -0,0 +1,18 @@
+const alunos = [
+    {nome: 'Fernando', nota: Math.floor(Math.random() * 10)},
+    {nome: 'Lucas', nota: Math.floor(Math.random() * 10)},
+    {nome: 'Davi', nota: Math.floor(Math.random() * 10)},
+    {nome: 'Isaque', nota: Math.floor(Math.random() * 10)},
+    {nome: 'Dalton', nota: Math.floor(Math.random() * 10)},
+    {nome: 'Vagner', nota: Math.floor(Math.random() * 10)},
+]
+
+console.log(alunos)
+const reprovados = alunos.filter(esudantes =>{
+    return esudantes.nota < 6
+   
+})
+for(let i=0;i < reprovados.length;i++){
+
+    console.log(reprovados[i].nome, reprovados[i].nota)
+}
---
+++ teste.js
@@ -0,0 +1,13 @@
+
+var num1 = 2,num2 = 3
+var soma
+
+soma = num1 + num2
+
+console.log(soma)
+//window.alert("teste")
+
+
+function clicar(){
+    alert("teste")
+}
